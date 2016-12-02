d11ccf8fbbef3a3d7c5f379235fe25cc9a1e5ae4
calebcox725 <caleb.cox@flatironschool.com>
2016-12-02 11:14:11 -0500

    Fix url to take /#/ out

--- map_project_front/src/index.js
+++ map_project_front/src/index.js
@@ -2,12 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import store from './store/store'
-import { Router, hashHistory } from 'react-router'
+import { Router, browserHistory } from 'react-router'
 import Routes from './routes'
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={hashHistory} routes={Routes} />
+    <Router history={browserHistory} routes={Routes} />
   </Provider>,
   document.getElementById('root')
 )
