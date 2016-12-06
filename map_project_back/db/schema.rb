8a0910815fa68abf72827b0373838e7ea0d7f649
calebcox725 <caleb.cox@flatironschool.com>
2016-12-06 09:57:12 -0500

    Establish has_many/belongs_to relationship between User and Marker. Fill out seeds.rb.

--- map_project_back/db/schema.rb
+++ map_project_back/db/schema.rb
@@ -26,8 +26,6 @@ ActiveRecord::Schema.define(version: 20161205221936) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "password_digest"
-    t.decimal  "lat"
-    t.decimal  "lng"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
--- map_project_back/db/seeds.rb
+++ map_project_back/db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+names = [
+  'Bobby',
+  'Hank',
+  'Peggy',
+  'Luanne',
+  'Ladybird',
+  'Dale',
+  'Nancy',
+  'Joseph',
+  'Bill',
+  'Boomhauer'
+]
+
+n_lat = 40.707
+s_lat = 40.703
+e_lng = -74.011
+w_lng = -74.017
+
+names.each { |name|
+  user = User.create(username: name, password: name)
+  lat = rand(s_lat..n_lat)
+  lng = rand(w_lng..e_lng)
+  marker = Marker.create(user_id: user.id, lat: lat, lng: lng)
+  marker.created_at = 1.days.ago
+  marker.save
+}
