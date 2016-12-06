e0c4fcbf91c98cf935060e7cad8b624de33888db
Robert Andrew Lin <github email address>
2016-12-06 14:06:26 -0500

    marker create and update functions add and fix

--- map_project_back/app/controllers/markers_controller.rb
+++ map_project_back/app/controllers/markers_controller.rb
@@ -3,19 +3,41 @@ class MarkersController < ApplicationController
   def create
     user_id = Auth.decode(params["jwt"])["user_id"]
     if user_id
-      marker = Marker.create(user_id: user_id, lat: params["data"]["lat"], lng: params["data"]["lng"])
-      render json: {
-        marker: {
-          user_id: marker.user_id,
-          lat: marker.lat,
-          lng: marker.lng,
-        }
-      }
+      marker = Marker.find_by("user_id = ? AND created_at >= ?", user_id, Time.zone.now.beginning_of_day)
+      if marker
+        update_marker(marker, params)
+      else
+        create_marker(user_id, params)
+      end
     else
       render json: {
-        error: "there shouldn't be any errors"
+        error: "some sort of jwt error"
       }
     end
+  end
+
+  private
+
+  def create_marker(user_id, params)
+    marker = Marker.create(user_id: user_id, lat: params["data"]["lat"], lng: params["data"]["lng"])
+    render json: {
+      marker: {
+        user_id: marker.user_id,
+        lat: marker.lat,
+        lng: marker.lng,
+      }
+    }
+  end
+
+  def update_marker(marker, params)
+    marker.update(lat: params["data"]["lat"], lng: params["data"]["lng"])
+    render json: {
+      marker: {
+        user_id: marker.user_id,
+        lat: marker.lat,
+        lng: marker.lng,
+      }
+    }
   end
 
 end
--- map_project_back/config/routes.rb
+++ map_project_back/config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :users, only: [:create]
   resources :sessions, only: [:create]
   resources :markers, only: [:create]
+
 end
