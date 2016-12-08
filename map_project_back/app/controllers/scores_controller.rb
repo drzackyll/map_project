335baa77f995a64f52e1595993c1504c736a0148
calebcox725 <caleb.cox@flatironschool.com>
2016-12-08 14:11:50 -0500

    Scores backened ready

--- map_project_back/app/controllers/scores_controller.rb
+++ map_project_back/app/controllers/scores_controller.rb
@@ -1,23 +1,40 @@
-render json: {
-  user: {
-    username: username,
-    days_survived: x,
-    humans_infected: y
-  },
-  scores: {
-    human: [
-      {
-        username: username,
-        days_survived: x
-      },
-      ...
-    ],
-    zombie: [
-      {
-        username: username,
-        humans_infected: y
-      },
-      ...
-    ]
-  }
-}
+class ScoresController < ApplicationController
+  def index
+    user_id = Auth.decode(params["jwt"])["user_id"]
+    if user_id
+      user = User.find(user_id)
+
+      human_scores = User.order(days_survived: :desc).limit(10)
+      human_scores_json = human_scores.each_with_object([]) do |user, array|
+        array << {
+          username: user.username,
+          days_survived: user.days_survived
+        }
+      end
+
+      zombie_scores = User.order(humans_infected: :desc).limit(10)
+      zombie_scores_json = human_scores.each_with_object([]) do |user, array|
+        array << {
+          username: user.username,
+          humans_infected: user.humans_infected
+        }
+      end
+
+      render json: {
+        user: {
+          username: user.username,
+          days_survived: user.days_survived,
+          humans_infected: user.humans_infected
+        },
+        scores: {
+          human: human_scores_json,
+          zombie: zombie_scores_json
+        }
+      }
+    else
+      render json: {
+        error: "some sort of jwt error"
+      }
+    end
+  end
+end
